ad961e9176c6a997832702ee2cd62117c68a5dac
sergeystarsky <46623490+sergeystarsky@users.noreply.github.com>
2019-01-12 22:21:41 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,8 @@
-var gulp = require ('gulp'); // require - подключение модуля который мы установили через nmp install
+var gulp = require ('gulp'), // require - подключение модуля который мы установили через nmp install
+    sass = require ('gulp-sass'), //поключаем препроцессинг sass
 
+ gulp.task('sass', function() {           //выполняем таск sass
+ 	return gulp.src('app/sass/main.sass')   //берем из папок файл main.sass
+	.pipe(sass())                           //выполняем таск sass   
+	.pipe(gulp.dest('app/css'))             // выводим результат в файл css(если указать main.css, то будет создан еще один файл, что не верно)
+ });
