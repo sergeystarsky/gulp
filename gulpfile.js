e491ee636e118643ab0833188f633758400fa88d
sergeystarsky <46623490+sergeystarsky@users.noreply.github.com>
2019-01-15 23:07:21 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-var gulp = require ('gulp'), // require - подключение модуля который мы установили через nmp install
-    sass = require ('gulp-sass'), //поключаем препроцессинг sass
-
+var 	gulp = require ('gulp'), // require - подключение модуля который мы установили через nmp install
+    	sass = require ('gulp-sass'), //поключаем препроцессинг sass
+	browserSync  = require('browser-sync'), //слежение за обновлениями
+    
+    
+    
  gulp.task('sass', function() {           //выполняем таск sass который преобразует sass в css
  	return gulp.src('app/sass/main.sass')   //берем из папок файл main.sass
 	.pipe(sass())                           //выполняем таск sass  (преобразуем sass в css посредством gulp-sass) 
@@ -11,3 +14,20 @@ var gulp = require ('gulp'), // require - подключение модуля к
 //return gulp.src('!app/sass/main.sass') данный файл исключается из выборки
 //return gulp.src('[!app/sass/main.sass', 'app/sass/**/*.sass]') через массив выбираем все файлы sass кроме main.sass
 //return gulp.src('app/sass/*.+(scss|sass)') выбираем все scss и все sass файлы в дериктории sass 
+
+//процесс слижения	
+gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'],  function() {//сначала стартует 'browser-sync', 'css-libs' (в обязаетльном порядке выполняет sass), далее watch, scripts запускаем до запуска сервера
+	gulp.watch('app/sass/**/*.sass', ['sass']); //следим за sass файлами во всех поддерикториях sass
+	gulp.watch('app/*.html', browserSync.reload); // следим за html файлами
+	gulp.watch('app/js/**/*.js', browserSync.reload);// следим за js во всех поддерикториях
+});
+
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false //убираем лого при перезагрузке
+	});
+});
